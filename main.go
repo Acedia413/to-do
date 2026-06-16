528f27c739d7ae31352dd448f4f29d6d70206d88
acedia413 <acedia413@gmail.com>
2026-06-16 20:15:13 +0200

    добавлена функция ввода задачи и описания реализовано добавление задачи в слайс

--- main.go
+++ main.go
@@ -1,1 +1,47 @@
-package todo 
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type task struct {
+	Name, Description string
+	Complete          bool
+}
+
+func scanTask(scanner *bufio.Scanner) task {
+	var t task
+	fmt.Println("Введите название задачи:")
+
+	if scanner.Scan() {
+		t.Name = scanner.Text()
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		fmt.Println("Ошибка ввода:", err)
+	}
+
+	fmt.Println("Введите описание задачи:")
+
+	if scanner.Scan() {
+		t.Description = scanner.Text()
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Ошибка ввода:", err)
+	}
+
+	return t
+}
+
+func main() {
+	var t task
+	tasks := make([]task, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	t = scanTask(scanner)
+	tasks = append(tasks, t)
+}
